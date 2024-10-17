5cc45495da31fc596b781fb32ed7594adf81d2a6
Andreas Nilsson <andreas.nilsson@hkr.se>
2024-10-17 07:58:16 +0200

    Add prefix as CLA

--- anonymous_filenames.py
+++ anonymous_filenames.py
@@ -3,20 +3,24 @@ from pathlib import Path
 from random import randint
 
 
-def generate_random_name(length):
+def generate_random_name(length, prefix):
     name = ''.join(f'{randint(0, 15):x}' for _ in range(length))        # Random hex string of length
-    return f'Task_{name}.pdf'
+    return f'{prefix}_{name}.pdf'
 
 
 def main():
-    if len(sys.argv) == 1:                                              # Use default length of 5 characters
-        length = 5
-    elif len(sys.argv) == 2:                                            # Use user specified length
+    length = 5
+    prefix = 'Task'
+    if len(sys.argv) == 2:                                              # User specifies length
         length = int(sys.argv[1])
+    elif len(sys.argv) == 3:
+        length = int(sys.argv[1])                                       # User specifies length and prefix
+        prefix = sys.argv[2]
     else:
-        print('Usage (2 options):')
+        print('Usage (3 options):')
         print('1) python3 anonymous_filenames.py')
         print('2) python3 anonymous_filenames.py <length>')
+        print('3) python3 anonymous_filenames.py <length> <prefix>')
         print()
         exit(1)
 
@@ -24,7 +28,7 @@ def main():
     pdf_files = filter(lambda f: f.suffix.lower() == '.pdf', files)     # Keep only pdf files
 
     for file in pdf_files:
-        file.rename(generate_random_name(length))
+        file.rename(generate_random_name(length, prefix))
 
 
 if __name__ == '__main__':
